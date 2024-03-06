a6cb37ff7e938188704899dc7be4c0065a97a2b8
CELSO MARCOS FURTADO <sn1006137@CFP127ALUNO.LOCAL>
2024-03-06 16:45:54 -0300

    Aula dia 06-03

---
+++ estado_imc.py
@@ -0,0 +1,22 @@
+# Módulo estado_imc
+
+def definir_estado_imc(imc):
+    estado = ""
+    if imc < 18.5:
+        estado = "Abaixo do Peso"
+    elif imc >= 18.5 and imc < 25.0:
+        estado = "Peso Ideal!"
+    elif imc >= 25.0 and imc < 30.0:
+        estado = "Levemente acima do peso"
+    elif imc >= 30.0 and imc < 35.0:
+        estado = "Obesidade Grau I"
+    elif imc >= 35.0 and imc < 40.0:
+        estado = "Obesidade Grau II"
+    else:
+        estado = "Obesidade Grau III"
+
+    print(f"O IMC do paciente é: {imc:.1f} - {estado}.")
+
+def calcular_imc(peso, altura):
+    imc = peso / altura**2
+    definir_estado_imc(imc)
---
+++ imc.py
@@ -0,0 +1,30 @@
+# módulo imc
+import estado_imc as calcular
+
+peso = 0
+altura = 0.0
+nome = ""
+
+nome = input("Qual o nome do paciente? ")
+
+erro = True
+
+while erro:
+    try:
+        peso = int(input("Qual o peso do paciente? "))
+        erro = False
+    except:
+        print("Valor do peso está incorreto!")
+
+
+erro = True
+while erro:
+    try:
+        altura = float(input("Qual a altura do paciente? "))
+        erro = False
+    except:
+        print("Valor da altura está incorreto!")
+
+calcular.calcular_imc(peso, altura)
+
+
